b820011c3ddfbf03cbf869a1e63dc3717903e61a
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-03 21:53:56 -0700

    pass

--- src/main/java/com/testdouble/gameoflife/ConwaySimulator.java
+++ src/main/java/com/testdouble/gameoflife/ConwaySimulator.java
@@ -4,6 +4,11 @@ package com.testdouble.gameoflife;
  Created by wpannell on 4/3/17.
  */
 public class ConwaySimulator {
+  private SeedWorld seedWorld;
+
+  private DisplayWorld displayWorld;
   public void simulate() {
+    World seed = seedWorld.seed();
+    displayWorld.display(seed);
   }
 }
